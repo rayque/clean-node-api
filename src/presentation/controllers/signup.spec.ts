cb924d6260a1b160efe0f55c69c4a65c870b050f
rayque <rayque.oliveira@gmail.com>
2020-06-15 13:34:17 -0400

    test: ensure SignUpController return an error ir no email provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,6 +12,20 @@ describe('Singup Controller', () => {
     }
     const htttpResponse = sut.handle(httpResquest)
     expect(htttpResponse.statusCode).toBe(400)
-    expect(htttpResponse.body).toEqual(new Error('Missing param: name'));
+    expect(htttpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpResquest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const htttpResponse = sut.handle(httpResquest)
+    expect(htttpResponse.statusCode).toBe(400)
+    expect(htttpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
